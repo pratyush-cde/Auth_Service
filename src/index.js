f10d89ff9f62f3cc4dbd2bb1d74823bba375a6a0
Pratyush <pratyush.0929@gmail.com>
2026-08-31 00:27:38 +0530

    added get user bu id in repository

--- src/index.js
+++ src/index.js
@@ -4,14 +4,18 @@ const app = express();
 const apiRoutes = require("./routes/index");
 const bodyParser = require("body-parser");
 
+const UserRepository = require("./repository/user-repository");
 const prepareAndStartServer = () => {
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
 
   app.use("/api", apiRoutes);
 
-  app.listen(PORT, () => {
+  app.listen(PORT, async () => {
     console.log(`server started at PORT: ${PORT}`);
+    const repo = new UserRepository();
+    const response = await repo.getById(5);
+    console.log(response);
   });
 };
 
